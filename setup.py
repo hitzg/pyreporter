e06786ab172cc67990a370b31f12a70fce3360c3
Gregory Hitz <hitzg@ethz.ch>
2014-11-26 22:29:40 +0100

    added simple setup script

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='pyreporter',
+      version='1.0',
+      description='Simple latex report generation tool',
+      author='Gregory Hitz',
+      py_modules=['pyreporter'],
+      )
